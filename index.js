ad4eb088767fdb1c9d0ce07328e6a0a28ed142fb
Anosh Taraporevala <110168536+anosh1000@users.noreply.github.com>
2026-04-22 14:59:53 -0700

    Commit Initial Files

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("new-deck").addEventListener("click", function() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
+
+// Functions are a first-class object in JavaScript
